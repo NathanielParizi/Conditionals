6596228c5ecdef25c9c470c2bff2679753934d48
justi <justi@192.168.254.3>
2018-07-08 21:20:46 -0400

    Conditional statements in C++ demonstration.

---
+++ Conditionals/Conditionals.cpp
@@ -0,0 +1,18 @@
+/*
+ * Conditionals.cpp
+ *
+ *  Created on: Jul 8, 2018
+ *      Author: Nathaniel-Joel Parizi
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "Hello Conditionals" << endl;
+
+	return 0;
+}
+
